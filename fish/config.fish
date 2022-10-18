f6985a84b5b650e81b47246fdde49cf9ecfc8204
lo-kiss <115636509+lo-kiss@users.noreply.github.com>
2022-10-18 12:13:25 +0300

    (lazy commit) archived i3 configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,21 @@
+date
 set fish_greeting
 
 # =============== Config Aliases ===============
 alias fishc="nvim ~/.config/fish/config.fish" 
 alias i3c="nvim ~/.config/i3/config"
-alias .='cd ~/.legendary-guacamole'
+alias .='cd ~/.legendary-guacamole && ll'
 
 # =============== Xbps Aliases ===============
 alias xi='sudo xbps-install -S'
+alias xu='sudo xbps-install -Su'
 alias xr='sudo xbps-remove -R'
 
 # =============== Other Aliases ===============
 alias lg='ll | rg' # 'list-grep'
 alias start_wpa='sudo wpa_supplicant -B -i wlp3s0 -c /etc/wpa_supplicant/wpa_supplicant-wlp3s0.conf'
 alias em="emacsclient -c -a 'nvim'"
+alias cll='clear && ll'
 alias cdl='cd && clear'
 alias cl="clear" 
 alias neo='neofetch'
